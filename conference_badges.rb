bd17d65a7ed55ccc618c8761b0ba36fbbdbce8db
Sarah Anderson <saranderson13@gmail.com>
2019-03-19 14:05:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def badge_maker(name_of_speaker)
-  "Hello, my name is #{name_of_speaker}"
+  "Hello, my name is #{name_of_speaker}."
 end
 
 def batch_badge_creator(array_of_speakers)
