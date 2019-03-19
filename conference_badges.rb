57a012d3e892bc04b7b31601bf6052ec71e62fbf
Sarah Anderson <saranderson13@gmail.com>
2019-03-19 14:12:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(array_of_speakers)
   batch_of_badges = []
   array_of_speakers.each do |speaker_name|
-    batch_of_badges.push("badge_maker(#{speaker_name}")
+    batch_of_badges.push(badge_maker(speaker_name))
   end
   batch_of_badges
 end
